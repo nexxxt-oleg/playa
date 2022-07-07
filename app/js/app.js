2439979cd8231c20c4ea742987f06d0fa492365b
Олег <nexxxt.07@gmail.com>
2022-07-07 17:36:01 +0300

    07.07 3

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 import '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import './dist/lazyload.min.js'
-//import './dist/gsap.js'
+
 import './dist/custom.js'
 
 
--- app/js/dist/custom.js
+++ app/js/dist/custom.js
@@ -1,14 +1,12 @@
-
-
 function animateFrom(elem, direction) {
     direction = direction | 1;
 
     var x = 0,
         y = direction * 100;
-    if(elem.classList.contains("gs_reveal_fromLeft")) {
+    if (elem.classList.contains("gs_reveal_fromLeft")) {
         x = -200;
         y = 0;
-    } else if(elem.classList.contains("gs_reveal_fromRight")) {
+    } else if (elem.classList.contains("gs_reveal_fromRight")) {
         x = 200;
         y = 0;
     }
@@ -27,6 +25,19 @@ function hide(elem) {
 }
 
 
+
+
+function activeMenu() {
+    gsap.fromTo(".playa__header__nav__item", {y: 100, autoAlpha: 0},
+        {
+            autoAlpha: 1,
+            ease: "power2.inOut",
+            y: 0,
+            delay: 0.4,
+            stagger: 0.1
+        });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
     let lazyLoadInstance = new LazyLoad({
@@ -34,40 +45,39 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
-
     gsap.registerPlugin(ScrollTrigger);
 
     var reveals = gsap.utils.toArray(".gs_reveal");
-    for(var i = 0; i < reveals.length; i++) {
+    for (var i = 0; i < reveals.length; i++) {
         (function () {
             var elem = reveals[i];
 
 
-                hide(elem); // assure that the element is hidden when scrolled into view
+            hide(elem); // assure that the element is hidden when scrolled into view
 
-                ScrollTrigger.create({
-                    trigger: elem,
-                    toggleClass: "active",
-                    onEnter: function () {
-                        animateFrom(elem)
-                    },
-                    onEnterBack: function () {
-                        //animateFrom(elem, -1)
-                    },
-                    onLeave: function () {
-                    } // assure that the element is hidden when scrolled into view
-                });
+            ScrollTrigger.create({
+                trigger: elem,
+                toggleClass: "active",
+                onEnter: function () {
+                    animateFrom(elem)
+                },
+                onEnterBack: function () {
+                    //animateFrom(elem, -1)
+                },
+                onLeave: function () {
+                } // assure that the element is hidden when scrolled into view
+            });
 
         })();
     }
 
     var reveal_img = gsap.utils.toArray(".gs_reveal_img");
-    for(var i = 0; i < reveal_img.length; i++) {
+    for (var i = 0; i < reveal_img.length; i++) {
         (function () {
             var elem = reveal_img[i];
             ScrollTrigger.create({
                 trigger: elem,
-                onEnter: function() {
+                onEnter: function () {
                     gsap.to(elem, {
                         duration: 1,
                         delay: 0.5,
@@ -83,12 +93,22 @@ document.addEventListener('DOMContentLoaded', () => {
                     });
 
                 },
-                onEnterBack: function() {
+                onEnterBack: function () {
 
                 },
-                onLeave: function() {  } // assure that the element is hidden when scrolled into view
+                onLeave: function () {
+                } // assure that the element is hidden when scrolled into view
             });
         })();
     }
+
+
+    document.getElementById('menuToggle').addEventListener('click', () => {
+        document.body.classList.toggle("menu-active")
+        if(document.body.classList.contains("menu-active")) {
+            activeMenu();
+        }
+    });
+
 
 });
