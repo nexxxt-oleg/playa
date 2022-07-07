43055a67e1e13b6ba2e74d0576d81fdc5645d961
Олег <nexxxt.07@gmail.com>
2022-07-07 13:35:54 +0300

    07.07 2

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,7 @@
-//import  '~/node_modules/swiper/swiper-bundle.min.js'
 import '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-//import '~/node_modules/@fancyapps/ui/dist/fancybox.umd.js'
-//import './smooth-scroll.polyfills.min.js'
-import './lazyload.min.js'
-
-//import './gsap.min.js'
-
-//import './imask.min.js'
+import './dist/lazyload.min.js'
+//import './dist/gsap.js'
+import './dist/custom.js'
 
 
 
