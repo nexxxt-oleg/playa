78af67e0509547689a9b0a6acbdf22ab234d570b
Олег <nexxxt.07@gmail.com>
2022-07-08 17:10:51 +0300

    08.7

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
 import '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import './dist/lazyload.min.js'
+import './dist/imask.min.js'
+//import './dist/air-datepicker.js'
 
 import './dist/custom.js'
 
--- app/js/dist/custom.js
+++ app/js/dist/custom.js
@@ -25,8 +25,6 @@ function hide(elem) {
 }
 
 
-
-
 function activeMenu() {
     gsap.fromTo(".playa__header__nav__item", {y: 100, autoAlpha: 0},
         {
@@ -44,71 +42,95 @@ document.addEventListener('DOMContentLoaded', () => {
         // Your custom settings go here
     });
 
+    if (window.screen.width > 990) {
+        gsap.registerPlugin(ScrollTrigger);
 
-    gsap.registerPlugin(ScrollTrigger);
-
-    var reveals = gsap.utils.toArray(".gs_reveal");
-    for (var i = 0; i < reveals.length; i++) {
-        (function () {
-            var elem = reveals[i];
+        var reveals = gsap.utils.toArray(".gs_reveal");
+        for (var i = 0; i < reveals.length; i++) {
+            (function () {
+                var elem = reveals[i];
 
 
-            hide(elem); // assure that the element is hidden when scrolled into view
+                hide(elem); // assure that the element is hidden when scrolled into view
 
-            ScrollTrigger.create({
-                trigger: elem,
-                toggleClass: "active",
-                onEnter: function () {
-                    animateFrom(elem)
-                },
-                onEnterBack: function () {
-                    //animateFrom(elem, -1)
-                },
-                onLeave: function () {
-                } // assure that the element is hidden when scrolled into view
-            });
+                ScrollTrigger.create({
+                    trigger: elem,
+                    toggleClass: "active",
+                    onEnter: function () {
+                        animateFrom(elem)
+                    },
+                    onEnterBack: function () {
+                        //animateFrom(elem, -1)
+                    },
+                    onLeave: function () {
+                    } // assure that the element is hidden when scrolled into view
+                });
 
-        })();
-    }
+            })();
+        }
 
-    var reveal_img = gsap.utils.toArray(".gs_reveal_img");
-    for (var i = 0; i < reveal_img.length; i++) {
-        (function () {
-            var elem = reveal_img[i];
-            ScrollTrigger.create({
-                trigger: elem,
-                onEnter: function () {
-                    gsap.to(elem, {
-                        duration: 1,
-                        delay: 0.5,
-                        scale: 1.05,
-                        ease: "sine.inOut"
-                    });
+        var reveal_img = gsap.utils.toArray(".gs_reveal_img");
+        for (var i = 0; i < reveal_img.length; i++) {
+            (function () {
+                var elem = reveal_img[i];
+                ScrollTrigger.create({
+                    trigger: elem,
+                    onEnter: function () {
+                        gsap.to(elem, {
+                            duration: 1,
+                            delay: 0.5,
+                            scale: 1.05,
+                            ease: "sine.inOut"
+                        });
 
-                    gsap.to(elem, {
-                        duration: 1,
-                        delay: 1.6,
-                        scale: 1,
-                        ease: "circ.inOut"
-                    });
+                        gsap.to(elem, {
+                            duration: 1,
+                            delay: 1.6,
+                            scale: 1,
+                            ease: "circ.inOut"
+                        });
 
-                },
-                onEnterBack: function () {
+                    },
+                    onEnterBack: function () {
 
-                },
-                onLeave: function () {
-                } // assure that the element is hidden when scrolled into view
-            });
-        })();
+                    },
+                    onLeave: function () {
+                    } // assure that the element is hidden when scrolled into view
+                });
+            })();
+        }
     }
 
 
     document.getElementById('menuToggle').addEventListener('click', () => {
         document.body.classList.toggle("menu-active")
-        if(document.body.classList.contains("menu-active")) {
+        if (document.body.classList.contains("menu-active")) {
             activeMenu();
         }
     });
 
+    IMask(document.getElementById('inpTel'), {
+        mask: '+{7}(000)000-00-00',
+        lazy: true
+    });
 
+    IMask(document.getElementById('inpTime'), {
+        mask: '00:00',
+        lazy: true
+    });
+
+    validate.init({
+        messageValueMissing: 'Пожалуйста, заполните это поле.',
+        messageValueMissingCheckbox: 'Пожалуйста, заполните это поле.',
+        disableSubmit: true,
+        onSubmit: function (form, fields) {
+            console.log(fields);
+        }
+    });
+
+    //new AirDatepicker('#inpDate');
+
+    new AirDatepicker('#inpDate', {
+
+    });
 });
